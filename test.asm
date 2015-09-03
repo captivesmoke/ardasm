5662c8fbea1221ea94c3b460dbf8b0c1fa0eaf12
John Bolt <jlb@captivesmoke.com>
2015-09-03 01:35:27 -0400

    Add interrupt vector table

--- test.asm
+++ test.asm
@@ -2,6 +2,51 @@
 	.include "/usr/share/avra/m328Pdef.inc"
 	.list
 
+;; Ard                               328                                Ard
+;; Reset        (PCINT14/RESET) PC6 |1  28| PC5 (ADC5/SCL/PCINT13)       A5
+;; D0(RX)         (PCINT16/RXD) PD0 |2  27| PC4 (ADC4/SDA/PCINT12)       A4
+;; D1(TX)         (PCINT17/TXD) PD1 |3  26| PC3 (ADC3/PCINT11)           A3
+;; D2            (PCINT18/INT0) PD2 |4  25| PC2 (ADC2/PCINT10)           A2
+;; D3(PWM)  (PCINT19/OC2B/INT1) PD3 |5  24| PC1 (ADC1/PCINT9)            A1
+;; D4          (PCINT20/XCK/T0) PD4 |6  23| PC0 (ADC0/PCINT8)            A0
+;; VCC                          VCC |7  22| GND                         GND
+;; GND                          GND |8  21| AREF                       AREF
+;; xtal    (PCINT6/XTAL1/TOSC1) PB6 |9  20| AVCC                        VCC
+;; xtal    (PCINT7/XTAL2/TOSC2) PB7 |10 19| PB5 (SCK/PCINT5)            D13
+;; D5(PWM)    (PCINT21/OC0B/T1) PD5 |11 18| PB4 (MISO/PCINT4)           D12
+;; D6(PWM)  (PCINT22/OC0A/AIN0) PD6 |12 17| PB3 (MOSI/OC2A/PCINT4) (PWM)D11
+;; D7            (PCINT23/AIN1) PD7 |13 16| PB2 (SS/OC1B/PCINT2)   (PWM)D10
+;; D8        (PCINT0/CLKO/ICP1) PB0 |14 15| PB1 (OC1A/PCINT1)       (PWM)D9
+
+;; Interrupt vector table
+	rjmp	Main		; RESET
+	reti			; INT0
+	reti			; INT1
+	reti			; PCINT0
+	reti			; PCINT1
+	reti			; PCINT2
+	reti			; WDT
+	reti			; TIMER2 COMPA
+	reti			; TIMER2 COMPB
+	reti			; TIMER2 OVF
+	reti			; TIMER1 CAPT
+	reti			; TIMER1 COMPA
+	reti			; TIMER1 COMPB
+	reti			; TIMER1 OVF
+	reti			; TIMER0 COMPA
+	reti			; TIMER0 COMPB
+	reti			; TIMER0 OVF
+	reti			; SPI STC
+	reti			; USART RX
+	reti			; USART UDRE
+	reti			; USART TX
+	reti			; ADC
+	reti			; EE READY
+	reti			; ANALOG COMP
+	reti			; TWI
+	reti			; SPM READY
+
+Main:
 	ldi	r16, 0b00100000
 	out	DDRB, r16
 Loop:
